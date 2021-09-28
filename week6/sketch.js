932f59fd1066429a4ae87e86a535f249243b25c0
Natalie-Wagley <natalie.wagley@mnsu.edu>
2021-09-28 17:33:41 -0500

    Adding draw program to webpage

---
+++ week6/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  var myCanvas = createCanvas(500, 400);
+  myCanvas.parent('canvasDiv');
+  background(68,137,161);
+}
+
+function draw() {
+textSize(30);
+textFont('ABeeZee-Regular');
+text('Natalie',mouseX,mouseY,40,40);
+fill(250,30,67);
+text('Natalie',mouseX, mouseY);
+fill(217,141,48);
+
+}
